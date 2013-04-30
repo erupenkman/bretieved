d582b754704caa951f4ca4c304272320f3c0f34a
Eru <penkman.eru@gmail.com>
2013-04-30 22:49:47 +1000

    Signed-off-by: Eru <penkman.eru@gmail.com>

--- server.js
+++ server.js
@@ -151,6 +151,6 @@ console.log('get all');
 
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server now listening on port " + app.get('port'));
 });
 
