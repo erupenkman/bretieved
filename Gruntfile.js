cc8992bfa353bfdf59a52ca64e6834a90e095213
eru <eru.penkman@gmail.com>
2013-11-20 22:41:07 +1100

    added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+	
+	grunt.loadNpmTasks('grunt-nodemon');
+	
+    // configurable paths
+    var yeomanConfig = {
+        app: 'app',
+        dist: 'dist'
+    };
+
+    grunt.initConfig({
+        yeoman: yeomanConfig,
+		nodemon: {
+			dev: {
+				options: {
+					file: 'server.js',
+					nodeArgs: ['--debug'],
+					env: {
+						PORT: '8282'
+					}
+				}
+			}
+		}
+    });
+
+    grunt.registerTask('server', function (target) {
+        return grunt.task.run([
+			'nodemon'
+		]);
+    });
+};
